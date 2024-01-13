b893fe4e1903aca446e5a1bfa1b69f302f75f8d6
csampakidis <csampakidis@uth.gr>
2024-01-13 20:39:05 +0200

    GraphicView method also handle the list of Node classes. Generate Network action of the File submenu adds the specified number of nodes with proper distances

--- graphicView.py
+++ graphicView.py
@@ -120,42 +120,88 @@ class GraphicView(QGraphicsView):
         self.scene.removeItem(link)
         self.edges.remove(link)
 
+        print(type(link), '', type(link.node1))
+
+        node1 = self.nodes_map[link.node1]
+        node2 = self.nodes_map[link.node2]
+
+        node1.neighbors.discard(node2)
+        node2.neighbors.discard(node1)
+
+        ids = set()
+        for node in node_list:
+            for neighb in node.neighbors:
+                ids.add(neighb.key)
+            print('Node', node.key, 'neighbors:', ids)
+            ids = set()
+        print()
+
     def addNode(self, pos):
-        new_node = Node(len(node_list) + 1)
+        if not node_list:
+            new_node = Node(1)
+        else:
+            new_node = Node(node_list[-1].key + 1)
         node_list.append(new_node)
 
         new_node_object = NodeObject(pos.x(), pos.y(), "NodeIcons\\node11.png", self.edges)
         # self.nodes.append(new_node_object)
-        self.scene.addItem(new_node)
+        self.scene.addItem(new_node_object)
 
-        self.nodes_map[new_node] = new_node_object
+        # self.nodes_map[new_node] = new_node_object
+        self.nodes_map[new_node_object] = new_node
 
+        ids = set()
         for node in node_list:
-            print('Node', node.key, 'neighbors:', node.neighbors)
+            for neighb in node.neighbors:
+                ids.add(neighb.key)
+            print('Node', node.key, 'neighbors:', ids)
+            ids = set()
+        print()
 
-    def deleteNode(self, node):
+    def deleteNode(self, node_object):
+        # Find the corresponding Node instance in the map
+        # node = next((key for key, value in self.nodes_map.items() if value == node_object), None)
+        node = self.nodes_map[node_object]
+
         # Remove only the edges connected to the deleted node
-        for edge in self.edges.copy():  # Use copy to avoid modifying the list during iteration
-            if edge.node1 == node or edge.node2 == node:
-                self.scene.removeItem(edge)
-                self.edges.remove(edge)
+        if node is not None:
+            for edge in self.edges.copy():  # Use copy to avoid modifying the list during iteration
+                if edge.node1 == node_object or edge.node2 == node_object:
+                    self.scene.removeItem(edge)
+                    self.edges.remove(edge)
 
         # Remove the node from the scene and the list of nodes
-        self.scene.removeItem(node)
-        #self.nodes.remove(node)
+        self.scene.removeItem(node_object)
+
+        # Remove the node from the map
+        del self.nodes_map[node_object]
+
+        # Remove the Node instance from node_list
         node_list.remove(node)
 
         # Clear the edges list from the deleted node
-        node.edges = []
-        node.neighbors = []
+        # node.edges = []
+        node.neighbors.clear()
+
+        # Remove the deleted node from the neighbor sets of other nodes
+        for other_node in node_list:
+            other_node.neighbors.discard(node)
+
+        ids = set()
+        for node in node_list:
+            for neighb in node.neighbors:
+                ids.add(neighb.key)
+            print('Node', node.key, 'neighbors:', ids)
+            ids = set()
+        print()
 
     def startAddingLink(self, node, pos):
+        print('startAddingLink', type(node))
         # Set the current node for linking
         self.adding_link_node = node
 
         # Connect the scene's mouse press event to start the link
-        # self.scene.mousePressEvent = self.addLinkStart
-        # self.scene.mousePressEvent = lambda event: self.addLinkStart(event, node)
+        # self.sceneMousePressEvent(pos)
         self.sceneMousePressEvent(pos)
 
     def sceneMousePressEvent(self, pos):
@@ -166,9 +212,26 @@ class GraphicView(QGraphicsView):
         # item = self.scene.itemAt(pos)
         item = self.scene.itemAt(pos.x(), pos.y(), self.transform())
         if isinstance(item, NodeObject) and item != self.adding_link_node:
+            # Use the item (NodeObject) directly to retrieve the associated Node
+            associated_node = self.nodes_map[item]
+            print('sceneMousePressEvent', 'self.adding_link_node', self.nodes_map[self.adding_link_node].key,
+                  'associated_node', associated_node.key)
+
+            self.nodes_map[self.adding_link_node].neighbors.add(associated_node)
+            associated_node.neighbors.add(self.nodes_map[self.adding_link_node])
+
             new_edge = EdgeObject(self.adding_link_node, item)
             self.edges.append(new_edge)
             self.scene.addItem(new_edge)
+
+            ids = set()
+            for node in node_list:
+                print(node.neighbors)
+                for neighb in node.neighbors:
+                    ids.add(neighb.key)
+                print('Node', node.key, 'neighbors:', ids)
+                ids = set()
+            print()
 
     def mousePressEvent(self, event):
         if self.adding_link_node is not None:
@@ -177,25 +240,22 @@ class GraphicView(QGraphicsView):
         else:
             super().mousePressEvent(event)
 
-    '''def addLinkStart(self, event):
-        print("Custom mousePressEvent is called")
-        if isinstance(event, QPointF):
-            # Convert scene coordinates to view coordinates
-            pos = self.mapFromScene(event)
-        else:
-            pos = event.pos()
-
-        item = self.scene.itemAt(pos)
-        if isinstance(item, NodeObject) and item != self.adding_link_node:
-            new_edge = EdgeObject(self.adding_link_node, item)
-            self.edges.append(new_edge)
-            self.scene.addItem(new_edge)
-
-        self.scene.mousePressEvent = QGraphicsScene.mousePressEvent
-        self.adding_link_node = None'''
-
     def clearAll(self):
         self.scene.clear()
         # self.nodes = []
         self.edges = []
         self.nodes_map = {}
+
+        # Reset the neighbor sets of all nodes
+        for node in node_list:
+            node.neighbors.clear()
+
+        node_list.clear()
+
+        ids = set()
+        for node in node_list:
+            for neighb in node.neighbors:
+                ids.add(neighb.key)
+            print('Node', node.key, 'neighbors:', ids)
+            ids = set()
+        print()
--- mainMenu.py
+++ mainMenu.py
@@ -1,7 +1,13 @@
+import math
+import random
+
 import networkx as nx
 from PyQt6.QtCore import QPointF, Qt
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QMenu, QGraphicsLineItem
+from shapely.geometry import Point
+from random import uniform
+
 
 from edgeObject import EdgeObject
 from netGenerationDialog import NetworkGenerationDialog
@@ -66,36 +72,86 @@ def generate_net(window):
         print(f"Network Density: {user_input['density']}")'''
         nodes = user_input['nodes']
         density = user_input['density']
+        min_distance = 70
 
-        # Generate Erdős-Rényi graph
+        '''# Generate Erdős-Rényi graph
         erdos_renyi_graph = nx.erdos_renyi_graph(nodes, density)
         pos = nx.spring_layout(erdos_renyi_graph)  # You can use other layout algorithms
 
         # Clear the existing scene and add nodes and edges to it
-        window.graphic_view.scene.clear()
+        window.graphic_view.scene.clear()'''
+
+        # Clear existing nodes and edges
+        window.graphic_view.clearAll()
+
+        # Generate Erdős-Rényi graph
+        erdos_renyi_graph = nx.erdos_renyi_graph(nodes, density)
+
+        # Get the bounds of the scene
+        scene_rect = window.graphic_view.sceneRect()
+
+        # Generate random positions for node positions with minimum distance constraint
+        i = 0
+        node_positions = []
+        for _ in range(nodes):
+            while True:
+                random_pos = QPointF(uniform(scene_rect.left(), scene_rect.right()),
+                                     uniform(scene_rect.top(), scene_rect.bottom()))
+                distances = [math.hypot(pos.x() - random_pos.x(), pos.y() - random_pos.y()) for pos in node_positions]
+                if all(distance >= min_distance for distance in distances):
+                    node_positions.append(random_pos)
+                    break
+
+        # Add nodes to GraphicView
+        for node, pos in zip(erdos_renyi_graph.nodes, node_positions):
+            window.graphic_view.addNode(pos)
 
         # Add nodes to the GraphicView scene
         for node in erdos_renyi_graph.nodes:
+            pass
             '''new_node = NodeObject(erdos_renyi_graph.nodes[node]['pos'][0],
                                   erdos_renyi_graph.nodes[node]['pos'][1],
                                   "NodeIcons\\node11.png", window.graphic_view.edges)'''
-            new_node = NodeObject(pos[node][0], pos[node][1], "NodeIcons\\node11.png", window.graphic_view.edges)
+            """new_node = NodeObject(pos[node][0], pos[node][1], "NodeIcons\\node11.png", window.graphic_view.edges)
             window.graphic_view.nodes.append(new_node)
-            window.graphic_view.scene.addItem(new_node)
+            window.graphic_view.scene.addItem(new_node)"""
+            """# Generate a random position within the scene
+            random_pos = QPointF(random.uniform(0, window.graphic_view.sceneRect().width()),
+                                 random.uniform(0, window.graphic_view.sceneRect().height()))"""
+
+            """# Generate a random position within the scene, ensuring minimum distance
+            random_pos = None
+            while random_pos is None or any(random_pos.toPoint().manhattanLength() < min_distance
+                                            for node_pos in window.graphic_view.nodes_map.values()):
+                random_pos = QPointF(random.uniform(0, window.graphic_view.sceneRect().width()),
+                                     random.uniform(0, window.graphic_view.sceneRect().height()))
+
+            # Call addNode to add both logical and visual representations
+            window.graphic_view.addNode(random_pos)"""
 
         # Add edges to the GraphicView scene
         for edge in erdos_renyi_graph.edges:
+            pass
             '''start_node = erdos_renyi_graph.nodes[edge[0]]['pos']
             end_node = erdos_renyi_graph.nodes[edge[1]]['pos']'''
-            start_node = pos[edge[0]]
+            """start_node = pos[edge[0]]
             end_node = pos[edge[1]]
             new_edge = EdgeObject(window.graphic_view.nodes[edge[0]], window.graphic_view.nodes[edge[1]])
             window.graphic_view.edges.append(new_edge)
-            window.graphic_view.scene.addItem(new_edge)
+            window.graphic_view.scene.addItem(new_edge)"""
+            """node1 = window.graphic_view.nodes[edge[0]]
+            node2 = window.graphic_view.nodes[edge[1]]
+            new_edge = EdgeObject(node1, node2)
+            window.graphic_view.edges.append(new_edge)
+            window.graphic_view.scene.addItem(new_edge)"""
 
-        # Adjust the view to fit the new scene
+        # Update the view
         window.graphic_view.fitInView(window.graphic_view.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
-        window.graphic_view.update()
+        window.graphic_view.updateView()
+
+        """# Adjust the view to fit the new scene
+        window.graphic_view.fitInView(window.graphic_view.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+        window.graphic_view.update()"""
 
         '''for node in erdos_renyi_graph.nodes:
             # Create a QGraphicsEllipseItem for each node
