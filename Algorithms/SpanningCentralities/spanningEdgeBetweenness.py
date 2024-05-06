f001dbd0a4a30cbeeda8e1721bff041ed72e6393
csampakidis <csampakidis@uth.gr>
2024-05-06 23:55:19 +0300

    Cleaned up code in spanningEdgeBetweenness.py

--- Algorithms/SpanningCentralities/spanningEdgeBetweenness.py
+++ Algorithms/SpanningCentralities/spanningEdgeBetweenness.py
@@ -1,7 +1,3 @@
-import math
-from collections import defaultdict
-from itertools import groupby
-
 import numpy as np
 from PyQt6.QtWidgets import QMessageBox
 
@@ -92,6 +88,18 @@ def msts_num(edges, nodes):
 
 
 def edge_msts_and_centrality(nodes, edges, laplacian_matrix, msts, spanning_betweenness_for_edges):
+    '''
+    Computes the number of MSTs containing edge = {u, v} which equals the det of Lij which is the laplacian matrix by
+    deleting rows u and v and columns u and v.
+    Then computes the Spanning Edge Betweenness centrality for every edge and stores it in spanning_betweenness_for_edges
+
+    Parameters:
+        - nodes (list of Node): The nodes of the graph.
+        - edges (list of Edge): The edges of the graph.
+        - laplacian_matrix (numpy.ndarray): The Laplacian matrix of the graph.
+        - msts (int): The number of MSTs in the graph.
+        - spanning_betweenness_for_edges (dictionary): Spanning Edge Betweenness centrality for every edge.
+    '''
     for edge in edges:
         i, j = nodes.index(edge.node1), nodes.index(edge.node2)
 
@@ -137,34 +145,12 @@ def spanEdgeBetw(window, node_list):
             msg.exec()
             return
 
-    """laplacian_matrix = get_laplacian_matrix(window.graphic_view.edges, node_list)
-
-    cofactor = np.linalg.det(laplacian_matrix[1:, 1:])
-
-    if np.isinf(cofactor):
-        laplacian_matrix = normalize_matrix(laplacian_matrix)
-
-        cofactor = np.linalg.det(laplacian_matrix[1:, 1:])"""
-
     msts, laplacian_matrix = msts_num(window.graphic_view.edges, node_list)
 
     # Initialize a dictionary to store results for each edge
     spanning_betweenness_for_edges = {}
 
     edge_msts_and_centrality(node_list, window.graphic_view.edges, laplacian_matrix, msts, spanning_betweenness_for_edges)
-
-    """for edge in window.graphic_view.edges:
-        i, j = node_list.index(edge.node1), node_list.index(edge.node2)
-
-        laplacian_ij = np.delete(laplacian_matrix, [i, j], axis=0)
-        laplacian_ij = np.delete(laplacian_ij, [i, j], axis=1)
-
-        # Number of spanning trees containing the edge
-        trees_for_edges = np.linalg.det(laplacian_ij)
-
-        # Store the result for the edge
-        spanning_betweenness_for_edges[tuple(sorted((edge.node1.key, edge.node2.key)))] = round(
-            trees_for_edges / cofactor, 4)"""
 
     window.side_table.update_table(spanning_betweenness_for_edges, 'Edge')
     window.side_label.setText('Algorithm: Spanning Edge Betweenness')
@@ -173,14 +159,36 @@ def spanEdgeBetw(window, node_list):
 
 class UnionFind:
     def __init__(self, vertices):
+        """
+        Initializes the UnionFind data structure with a dictionary mapping each vertex to itself.
+
+        Parameters:
+            - vertices (list of Node): A list of vertices.
+        """
         self.parent = {v: v for v in vertices}
 
     def find(self, vertex):
+        """
+        Finds the representative (root) of the set that the given vertex belongs to using path compression.
+
+        Parameters:
+            - vertex (Node): The vertex to find.
+
+        Returns:
+            - parent[vertex] (Node): The representative (root) of the set that the vertex belongs to.
+        """
         if self.parent[vertex] != vertex:
             self.parent[vertex] = self.find(self.parent[vertex])
         return self.parent[vertex]
 
     def union(self, u, v):
+        """
+        Unites the sets that contain vertices u and v by setting the root of one as the parent of the other.
+
+        Patameters:
+            - u: A vertex.
+            - v: A vertex.
+        """
         root_u = self.find(u)
         root_v = self.find(v)
         self.parent[root_v] = root_u
@@ -213,7 +221,6 @@ def spanEdgeBetwWeighted(window, node_list):
     edges = sorted(window.graphic_view.edges, key=lambda _edge: _edge.weight)  # Sort edges by weight
     vertices = node_list.copy()
     uf = UnionFind(vertices)
-    # msts = []
     i = 0
     tg = 1
     spanning_betweenness_for_edges = {tuple(sorted((edge.node1.key, edge.node2.key))): 0.0 for edge in edges}
@@ -226,16 +233,8 @@ def spanEdgeBetwWeighted(window, node_list):
             same_weight_edges.append(edges[j])
             j += 1
 
-        """for edge in same_weight_edges:
-            print(f'weighted ({edge.node1.key}, {edge.node2.key})')"""
-
         # Find connected components in the graph formed by mst_edges
         connected_components = find_connected_components(same_weight_edges)
-
-        """for component in connected_components:
-            for edge in component:
-                print(f'component edge ({edge.node1.key}, {edge.node2.key})', end=' ')
-            print()"""
 
         for component in connected_components:
             comp_nodes = set()
@@ -250,14 +249,9 @@ def spanEdgeBetwWeighted(window, node_list):
             if brk:
                 continue
             comp_nodes = list(comp_nodes)
-            """print('component nodes:', end=' ')
-            for node in comp_nodes:
-                print(node.key, end=' ')
-            print()"""
+
             tc, laplacian_matrix = msts_num(component, comp_nodes)
-            # print('tc:', tc)
             tg *= tc
-            # laplacian_matrix = get_laplacian_matrix(component, comp_nodes)
             edge_msts_and_centrality(comp_nodes, component, laplacian_matrix, tc, spanning_betweenness_for_edges)
 
         mst_edges = []
@@ -265,31 +259,6 @@ def spanEdgeBetwWeighted(window, node_list):
             if uf.find(edge.node1) != uf.find(edge.node2):
                 mst_edges.append(edge)
                 uf.union(edge.node1, edge.node2)
-                # print(f'added ({edge.node1.key}, {edge.node2.key})')
-                # print(f'set parent of {edge.node1.key} to {uf.find(edge.node1).key}, {edge.node2.key} to {uf.find(edge.node2).key}')
-
-        """bnodes = node_list.copy()
-        print('bnodes')
-        for node in bnodes:
-            print(node.key, end=' ')
-        for node in vertices:
-            if same_weight_edges[0].weight == 42.0 and node.key == 8:
-                print('parent of', node.key, ':', uf.find(node).key)
-            if uf.find(node) != node:
-                bnodes.remove(node)
-                print('removed', node.key, end=' ')
-            if node.key == 6 or node.key == 8:
-                print('parent', node.key, ':', uf.find(node).key)
-        if len(bnodes) == 1:
-            break
-
-        # Run Kruskal's algorithm for each set of edges with the same weight
-        msts.append(mst_edges)
-        print('msts:')
-        for mst in msts:
-            for edge in mst:
-                print(f'({edge.node1.key}, {edge.node2.key})', end=' ')
-            print()"""
 
         # Skip to the next set of edges with different weights
         i = j
